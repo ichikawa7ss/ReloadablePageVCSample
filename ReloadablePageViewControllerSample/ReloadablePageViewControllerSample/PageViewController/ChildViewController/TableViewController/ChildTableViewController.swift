b7b00edbbe0830bb4a9a34f3906436e1c81032b1
ichikawa7ss <i2s7.chacha@gmail.com>
2020-10-12 07:32:04 +0900

    Trace DelitablePageViewController project.

---
+++ ReloadablePageViewControllerSample/ReloadablePageViewControllerSample/PageViewController/ChildViewController/TableViewController/ChildTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChildTableViewController.swift
+//  ReloadablePageViewControllerSample
+//
+//  Created by ichikawa on 2020/10/12.
+//  Copyright © 2020 ichikawa. All rights reserved.
+//
+
+import Foundation
---
+++ ReloadablePageViewControllerSample/ReloadablePageViewControllerSample/PageViewController/PageViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PageViewController.swift
+//  ReloadablePageViewControllerSample
+//
+//  Created by ichikawa on 2020/10/12.
+//  Copyright © 2020 ichikawa. All rights reserved.
+//
+
+import Foundation
